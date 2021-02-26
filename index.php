a5468d0710c51462058c813e6e3bbfbf6d21da76
temirlan23 <76035299+temirlan23@users.noreply.github.com>
2021-02-26 11:56:25 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,24 @@
   <title>Тестируем PHP</title>
  </head>
  <body>
- <?php echo '<p>temirlan good boy</p>'; ?>
+  	<h1>Hi this is site <br><em>Nurislam so good teacher</em> :)</h1>
+	<p><em> HI what are you doing now  </em></p>
+	<a href="https://www.youtube.com/watch?v=VljpP4csbOc" ><button style="color: purple">take me </button></a>
+	<div class="hello"></div>
+	
+
+	<iframe width="222" height="222"
+src="https://www.youtube.com/watch?v=hq3pLeW5qB4">
+</iframe>
+	 <ul  style="text-align: center;  color:green ">  
+	 	<li >Temirlan</li> 
+	 	<li>Aidana</li>
+	 	<li>altya</li>
+	 	<li>Akyl</li>
+	 </ul>
+
+
+
+ <?php echo '<p>jiraia</p>'; ?>
  </body>
 </html>
