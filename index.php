6a65e1aa24e699830ccb98e6fc221410dc8b7e1b
temirlan23 <76035299+temirlan23@users.noreply.github.com>
2021-03-18 09:24:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	</head>
 <body style="color:white;">
         <p style="font-size:120px:float:left">&#128514;</p>
-	<div><img src="5.png" width="120" height="120" style="vertical-align:right; margin-top:0px;  border-radius: 50%; float: right;" border="2";>
+	/*<div><img src="5.png" width="120" height="120" style="vertical-align:right; margin-top:0px;  border-radius: 50%; float: right;" border="2";>*/
 	<h1 >Temirlan Dusumbaev</h1>
 	<h2 >EDUCATION</h2>
 	<div  > <em>IT& Business College of Alatoo International University<br>
