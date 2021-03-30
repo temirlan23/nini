d67f10a54ef780a7390977d3b5bc5a19a4dfca9b
temirlan23 <76035299+temirlan23@users.noreply.github.com>
2021-03-30 18:23:22 +0300

    Update main.php

--- main.php
+++ main.php
@@ -21,66 +21,74 @@ Study years - 2018-2021</em>
         </ul>
 		</div>
 	
+ <section class="orange-fade p-5 margin-top-xl pos-r">
+  <div class="container">
+   	<div class="row">
+		<div class="col-sm-12">
+        <h2 class="text-center font-weight-bold text-white">Our Customers are Seeing Big Results</h2>
+        <p></p>
+       <div class="mt-5 pos-r">
+          <div class="carousel-controls testimonial-carousel-controls">
+            <div class="control prev"><i class="fa fa-chevron-left text-white">&nbsp;</i></div>
+            <div class="control next"><i class="fa fa-chevron-right text-white">&nbsp;</i></div>
+          </div>
+          <div class="testimonial-carousel">
+            <div class="one-slide white">
+              <div class="testimonial w-100 h-100  p-3 text-center">
+                <div class="message text-center text-gray">I have been working with Solodev for nearly 4 years and I couldn&#39;t be happier with the results!</div>
+                <div class="separator">&nbsp;</div>
+                <div class="brand"><img alt="Florida Dairy Farmers" src="https://raw.githubusercontent.com/solodev/slider-boxes/master/images/img-1.png" class="mx-auto"/></div>
+              </div>
+            </div>
+            <div class="one-slide white">
+              <div class="testimonial w-100 h-100  p-3 text-center">
+                <div class="message text-center text-gray">The interface is easy for a novice user to make updates just as easily as an experienced developer.</div>
+                <div class="separator">&nbsp;</div>
+                <div class="brand"><img alt="Florida Department of Education" src="https://raw.githubusercontent.com/solodev/slider-boxes/master/images/img-2.png" class="mx-auto"/></div>
+              </div>
+            </div>
+            <div class="one-slide white">
+              <div class="testimonial w-100 h-100  p-3 text-center">
+                <div class="message text-center text-gray">Solodev is a great company to partner with! We are extremely happy with the software, service, and support.</div>
+                <div class="separator">&nbsp;</div>
+                <div class="brand"><img alt="Seminole County" src="https://raw.githubusercontent.com/solodev/slider-boxes/master/images/img-3.png" class="mx-auto" /></div>
+              </div>
+            </div>
+            <div class="one-slide white">
+              <div class="testimonial w-100 h-100  p-3 text-center">
+                <div class="message text-center text-gray">If you are looking for a great CMS company then Solodev is the right choice.</div>
+                <div class="separator">&nbsp;</div>
+                <div class="brand"><img alt="Lynx" src="https://raw.githubusercontent.com/solodev/slider-boxes/master/images/img-4.png" class="mx-auto" /></div>
+              </div>
+            </div>
+            <div class="one-slide white">
+              <div class="testimonial w-100 h-100  p-3 text-center">
+                <div class="message text-center text-gray">Everyone on the Solodev team is very knowledgeable and they have been always been very responsive.</div>
+                <div class="separator">&nbsp;</div>
+                <div class="brand"><img alt="Miami Beach VCA" src="https://raw.githubusercontent.com/solodev/slider-boxes/master/images/img-5.png" class="mx-auto" /></div>
+              </div>
+            </div>
+            <div class="one-slide white">
+              <div class="testimonial w-100 h-100  p-3 text-center">
+                <div class="message text-center text-gray">Solodev has been able to successfully keep us at the forefront of our industry and continually meet our ever adapting needs.</div>
+                <div class="separator">&nbsp;</div>
+                <div class="brand"><img alt="Oneblood" src="https://raw.githubusercontent.com/solodev/slider-boxes/master/images/img-6.png" class="mx-auto" /></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
  
- <div>
-	
 
 
-    <a  style="color:Cornsilk;" href="">telegram</a><br>
-    <a style="color:Cornsilk;" target="_blank"  href="https://vk.com/ueban023">VK</a><br>
-	     <a target="_blank" href="https://www.instagram.com/vipp633/" title="Follow me :)">
-<img src="3.png" alt="Instagram" style="width:70px;height:70px;">
-	 </a>
-    <a style="color:Cornsilk;" target="_blank"  href="https://www.instagram.com/vipp633/" >instogram</a>
-	 </div>
-		<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
-
-<div id="id01" class="modal">
   
-  <form class="modal-content animate" action="" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img_avatar2.png" alt="Avatar" class="avatar">
-    </div>
-
-    <div class="container">
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" id="fname" required>
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" id="lname" required>
-        
-      <button type="submit" onclick="myFunction()">Login</button>
-      <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-      </label>
-    </div>
-	  
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-	    
-	 <script lauguage="javascript">
-	function myFunction(){
-      var a = document.getElementById("fname").value
-      var b = document.getElementById("lname").value
-      var form = document.querySelector("form")
-
-       if (a=="terminal" && b == "termilan"){
-        form.setAttribute('action', 'https://www.youtube.com/watch?v=yY7iGa4t9-I');
-       }else{
-        alert('permission denied')
-       }
-	}
 
 
-</script>
-    </div>
-  </form>
-</div>
  
-<img  width="" height="">
+
 	
 	<?php echo '<p></p>' ?>
 
