5c41ce50d25e349f8ca70a7defc9e798828664af
temirlan23 <76035299+temirlan23@users.noreply.github.com>
2021-03-31 17:38:51 +0300

    Update main.php

--- main.php
+++ main.php
@@ -3,32 +3,8 @@
 <head>
 	<title>terminatoor</title>
 <link rel="stylesheet" type="text/css" href="style.css" />
-	<script src="script.js"></script>
-		<script type="text/javascript">
-$(document).ready(function(){
-    $('.customer-logos').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-});
-</script>
-	</head>
+	<meta charset="utf-8">
+</head>
 <body style="color:white;">
 	
 
@@ -50,34 +26,45 @@ Study years - 2018-2021</em>
 	    <li>Basic Programming Knowledge in C++ and Python.</li>
         </ul>
 		</div>
-	
-<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+	<div class="all">
+        <input checked type="radio" name="respond" id="desktop">
+        <article>
+            <input checked  type="radio" name="slider" id="swich1">
+            <input  type="radio" name="slider" id="swich2">
+            <input  type="radio" name="slider" id="swich3">
+            <input  type="radio" name="slider" id="swich4">
+            <input  type="radio" name="slider" id="swich5">  
 
-<div class="container">
-  <h2>Our  Partners</h2>
-   <section class="customer-logos slider">
-      <div class="slide"><img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/retro-label-on-rustic-background_82147503374.jpg"></div>
-   </section>
-</div>
- 
+            <div id="slides">
+                <div id="overflow">
+                    <div class="image">
+                        <article><img src="1.jpg"></article>
+                        <article><img src="2.jpg"></article>
+                        <article><img src="3.jpg"></article>
+                        <article><img src="4.jpg"></article>
+                        <article><img src="5.jpg"></article>
+                    </div>
+                </div>>
+            </div> 
+            <div id="controls">
+                <label for="switch1"></label>
+                <label for="switch2"></label>
+                <label for="switch3"></label>
+                <label for="switch4"></label>
+                <label for="switch5"></label>
+                
+            </div> 
+              <div id="active">
+                <label for="switch1"></label>
+                <label for="switch2"></label>
+                <label for="switch3"></label>
+                <label for="switch4"></label>
+                <label for="switch5"></label>
+                
+            </div> 
+        </article>
+    </div>
 
-
-  
-
-
- 
-
-	
 	<?php echo '<p></p>' ?>
 
 
