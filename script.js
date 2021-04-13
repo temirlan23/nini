6a6947cc1a8868b61706f3177f53f6e8796d8614
temirlan23 <76035299+temirlan23@users.noreply.github.com>
2021-04-13 08:42:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-$(document).ready(function() {
-    $(".testimonial-carousel").slick({
-        infinite: !0,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: !1,
-        arrows:true,
-        prevArrow: $(".testimonial-carousel-controls .prev"),
-        nextArrow: $(".testimonial-carousel-controls .next"),
-        responsive: [{
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2
-            }
-        }, {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1
-            }
-        }]
-    });
-});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
